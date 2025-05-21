db90acd1135fc3956c9f6440d77ef7441ac6b125
Atanas Andonov <andonov.atanas98@gmail.com>
2025-05-21 22:53:50 +0300

    Exercise 7

---
+++ 01. Programming Basics with Python - January 2025/04_Exercise_First_Steps_In_Coding/07_Food_Delivery.py
@@ -0,0 +1,8 @@
+chicken = int(input()) * 10.35
+fish = int(input()) * 12.40
+vegan = int(input()) * 8.15
+sum_menus = chicken + fish + vegan
+desert = sum_menus * 0.2
+
+result = sum_menus + desert + 2.50
+print(float(result))
