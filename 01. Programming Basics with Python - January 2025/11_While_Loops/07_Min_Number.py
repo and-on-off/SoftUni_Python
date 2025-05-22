daa9a090a7d05ff486d560a5443c5d2e864d159d
Atanas Andonov <andonov.atanas98@gmail.com>
2025-05-22 21:51:37 +0300

    While Loops - Exercise 7

---
+++ 01. Programming Basics with Python - January 2025/11_While_Loops/07_Min_Number.py
@@ -0,0 +1,16 @@
+import sys
+
+max_number = sys.maxsize
+
+while True:
+    number = input()
+
+    if number == "Stop":
+        break
+    else:
+        number = float(number)
+
+    if max_number >= number:
+        max_number = number
+
+print(int(max_number))
