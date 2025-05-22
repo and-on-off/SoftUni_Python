96fd3430042c2e423a9ae790495a5e736a571ee4
Atanas Andonov <andonov.atanas98@gmail.com>
2025-05-22 22:02:16 +0300

    Fundamentals Introduction - Exercise 6
    Revise Basic Syntax, Conditional Statements and Loops

---
+++ 02. Programming Fundamentals with Python - May 2025/05. Basic Syntax, Conditional Statements and Loops - Lab/06_Shopping.py
@@ -0,0 +1,17 @@
+budget = int(input())
+total_price = 0
+
+while True:
+    command = input()
+
+    if command == "End":
+        print(f"You bought everything needed.")
+        break
+
+    price = float(command)
+
+    if total_price + price > budget:
+        print(f"You went in overdraft!")
+        break
+
+    total_price += price
