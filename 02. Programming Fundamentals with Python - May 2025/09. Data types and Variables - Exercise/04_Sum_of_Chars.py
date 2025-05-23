6cd4dbbb94c82e7d08503428dfc08aece68cfc9c
Atanas Andonov <andonov.atanas98@gmail.com>
2025-05-23 19:05:05 +0300

    Exercise Data types and Variables - Exercise 4

---
+++ 02. Programming Fundamentals with Python - May 2025/09. Data types and Variables - Exercise/04_Sum_of_Chars.py
@@ -0,0 +1,8 @@
+number_of_chars = int(input())
+total_sum = 0
+
+for char in range(number_of_chars):
+    current_char = input()
+    total_sum += ord(current_char)
+
+print(f"The sum equals: {total_sum}")
