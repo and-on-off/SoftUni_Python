9ce4567a4b1fc419cc030225804d232e8edfd9e7
Atanas Andonov <andonov.atanas98@gmail.com>
2025-05-23 19:47:00 +0300

    Added new variant.

--- 02. Programming Fundamentals with Python - May 2025/09. Data types and Variables - Exercise/06_Triples_of_Latin_Letters.py
+++ 02. Programming Fundamentals with Python - May 2025/09. Data types and Variables - Exercise/06_Triples_of_Latin_Letters.py
@@ -1,6 +1,15 @@
+# Variant 1
 n = int(input())
 
 for i in range(0, n):
     for k in range(0, n):
         for j in range(0, n):
             print(f"{chr(97 + i)}{chr(97 + k)}{chr(97 + j)}")
+
+# Variant 2
+n = int(input())
+
+for i in range(97, 97 + n):
+    for k in range(97, 97 + n):
+        for j in range(97, 97 + n):
+            print(f"{chr(i)}{chr(k)}{chr(j)}")
