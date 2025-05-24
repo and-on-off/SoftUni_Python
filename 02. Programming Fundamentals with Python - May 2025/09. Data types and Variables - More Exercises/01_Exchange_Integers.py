b9e44b0ca96b707b03cfa26f0d031d480ccdcb0c
Atanas Andonov <andonov.atanas98@gmail.com>
2025-05-24 21:42:00 +0300

    Researching Tuple

--- 02. Programming Fundamentals with Python - May 2025/09. Data types and Variables - More Exercises/01_Exchange_Integers.py
+++ 02. Programming Fundamentals with Python - May 2025/09. Data types and Variables - More Exercises/01_Exchange_Integers.py
@@ -25,3 +25,15 @@ print(f"b = {b}")
 print(f"After:")
 print(f"a = {b}")
 print(f"b = {a}")
+
+# Variant 3
+a = int(input())
+b = int(input())
+
+print(f"Before:")
+print(f"a = {a}")
+print(f"b = {b}")
+a, b = b, a
+print(f"After:")
+print(f"a = {a}")
+print(f"b = {b}")
