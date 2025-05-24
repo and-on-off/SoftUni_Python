79c48f6ed033259c875b262f11b7a923b2658efd
Atanas Andonov <andonov.atanas98@gmail.com>
2025-05-24 21:38:17 +0300

    Added second variant.

--- 02. Programming Fundamentals with Python - May 2025/09. Data types and Variables - More Exercises/01_Exchange_Integers.py
+++ 02. Programming Fundamentals with Python - May 2025/09. Data types and Variables - More Exercises/01_Exchange_Integers.py
@@ -1,3 +1,4 @@
+# Variant 1
 number_one = int(input())
 number_two = int(input())
 
@@ -12,3 +13,15 @@ number_two = temp
 print(f"After:")
 print(f"a = {number_one}")
 print(f"b = {number_two}")
+
+
+# Variant 2 - works for the given problem
+a = int(input())
+b = int(input())
+
+print(f"Before:")
+print(f"a = {a}")
+print(f"b = {b}")
+print(f"After:")
+print(f"a = {b}")
+print(f"b = {a}")
