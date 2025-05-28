b696cd72f5041b65cf46795e466bbb466069e2de
Atanas Andonov <andonov.atanas98@gmail.com>
2025-05-28 19:46:05 +0300

    Added second variant

--- 02. Programming Fundamentals with Python - May 2025/11. List Basics/01_Strange_Zoo.py
+++ 02. Programming Fundamentals with Python - May 2025/11. List Basics/01_Strange_Zoo.py
@@ -1,7 +1,21 @@
+# Variant 1
+
 tail = input()
 body = input()
 head = input()
 
 meerkat = [head, body, tail]
+
+print(meerkat)
+
+# Variant 2
+
+meerkat = []
+
+for i in range(3):
+    data = input()
+    meerkat.append(data)
+
+meerkat[0], meerkat[2] = meerkat[2], meerkat[0]
 
 print(meerkat)
