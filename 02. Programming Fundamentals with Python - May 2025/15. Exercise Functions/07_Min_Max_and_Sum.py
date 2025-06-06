9f457ba018e89515492d67c6bc7b851eb046b4db
Atanas Andonov <andonov.atanas98@gmail.com>
2025-06-06 23:00:57 +0300

    Exercise Functions - Exercise 7

---
+++ 02. Programming Fundamentals with Python - May 2025/15. Exercise Functions/07_Min_Max_and_Sum.py
@@ -0,0 +1,5 @@
+sequence = list(map(int, input().split()))
+
+print(f"The minimum number is {min(sequence)}\n"
+      f"The maximum number is {max(sequence)}\n"
+      f"The sum number is: {sum(sequence)}")
